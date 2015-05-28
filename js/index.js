8b069da9f04251e4c663fe60513a8ab789b108af
Melanie B <melaniebatenchuk@Melanies-MacBook-Pro.local>
2015-05-27 21:03:32 -0400

    Cloned project to desktop
    All files copied to GH

---
+++ js/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  //Initiate Masonry on the div with ID imageGrid
+  $('#imageGrid').masonry({
+   //Make each div with the class item a Masonry block	
+   itemSelector: '.item'
+  });
+});
+
+$("sticky_nav").stick_in_parent();
+	.on("sticky_kit:stick")
+	
+	});
+
