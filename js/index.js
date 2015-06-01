7bdfb9332700908ac9d93aef61f727e99d57b646
Melanie B <melaniebatenchuk@Melanies-MacBook-Pro.local>
2015-06-01 13:12:45 -0400

    Updated classes for animonscroll

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ $("sticky_nav").stick_in_parent();
 	});
 });
 
-AnimOnScroll( document.getElementById( 'item' ), {
+AnimOnScroll( document.getElementById( 'imageGrid' ), {
 		minDuration : 0.4,
 		maxDuration : 0.7,
 		viewportFactor : 0.2
